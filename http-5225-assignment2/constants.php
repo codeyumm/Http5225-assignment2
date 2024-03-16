73bea34329c74d7cd66b249d682d87ba350adc60
codeyumm <pvashi006@gmail.com>
2024-03-16 16:58:10 -0400

    First commit

---
+++ http-5225-assignment2/constants.php
@@ -0,0 +1,5 @@
+<?php 
+    define('BASE_PATH', __DIR__);
+    define('PUBLIC_PATH', BASE_PATH . '\public');
+    $cssPath =  PUBLIC_PATH . '\style.css';
+?>
---
+++ http-5225-assignment2/src/shared/signup.php
@@ -0,0 +1,3 @@
+
+<!-- inclue header from shared layout -->
+<?php include('../layout/shared/header.php'); ?>
