32894f714c27223eb617de226d7a95aabab3e776
codeyumm <pvashi006@gmail.com>
2024-03-16 19:27:57 -0400

    Added signup

--- http-5225-assignment2/src/signup.php
+++ http-5225-assignment2/src/signup.php
@@ -19,65 +19,46 @@
     include($sharedLayout . '\nav.php');
 
 
-
-    // check if request coming is post or not and is coming from singup form
-    if(  isset($_POST['isFromSignup']) ){
-        
-        
-
-    }
-
-    // $conn = ConnectDB();
-
-    // $query = "SELECT food_item_id, food_item_name, image_url FROM food_items";
-
-    // $result = mysqli_query($conn, $query);
-
-
-    //for testing response from db
-        // echo '<pre>';
-        // echo print_r($result); 
-        // echo '</pre>';
 ?>
 
 <div class="container-fluid">
     <div class="row justify-content-center align-items-center" style="height: 100vh;">
         <div class="col-md-4">
             <div class="reg-form-container px-4">
-                <form action="" method="POST" id="reg-form" class="p-4">
+                <form action="./components/process-signup.php" method="POST" id="reg-form" class="p-4" novalidate>
 
                     <div class="form-group mb-4">
                         <label for="firstName">First Name</label>
                         <div class="input-group">
-                            <input type="text" class="form-control" id="firstName" placeholder="Enter first name">
+                            <input type="text" class="form-control" name="firstName" id="firstName" placeholder="Enter first name" required>
                         </div>
                     </div>
 
                     <div class="form-group mb-4">
                         <label for="lastName">Last Name</label>
                         <div class="input-group">
-                            <input type="text" class="form-control" id="lastName" placeholder="Enter last name">
+                            <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Enter last name" required>
                         </div>
                     </div>
 
                     <div class="form-group mb-4">
                         <label for="email">Email address</label>
                         <div class="input-group">
-                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" required>
                         </div>
                     </div>
 
                     <div class="form-group mb-4">
                         <label for="password">Password</label>
                         <div class="input-group">
-                            <input type="password" class="form-control" id="password" placeholder="Enter password">
+                            <input type="password" class="form-control" name="password" id="password" placeholder="Enter password" required>
                         </div>
                     </div>
 
                     <div class="form-group mb-4">
                         <label for="confirmPassword">Confirm Password</label>
                         <div class="input-group">
-                            <input type="password" class="form-control" id="confirmPassword" placeholder=" Re-enter password">
+                            <input type="password" class="form-control" name="confirmPassword" id="confirmPassword" placeholder=" Re-enter password" required>
                         </div>
                     </div>
 
